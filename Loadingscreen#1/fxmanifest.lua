68cb9acf6d2d4cf7967573721a521787dcca7a13
Pintta <69728770+Pintta@users.noreply.github.com>
2024-05-29 03:20:29 +0300

    Add files via upload

---
+++ Loadingscreen#1/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+files {
+    'index.html',
+    'style/*.css',
+    'logo.png',
+    'song/*.mp3'
+}
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+loadscreen_cursor 'yes'
